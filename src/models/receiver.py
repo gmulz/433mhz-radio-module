55761a53323152c0e826eef5476ee722b48dd06d
Grant Mulitz <gamulitz@gmail.com>
2025-06-19 00:53:26 -0400

    continuous receiver

--- src/models/receiver.py
+++ src/models/receiver.py
@@ -15,6 +15,7 @@ from viam.components.board import Board
 from viam.utils import ValueTypes, SensorReading
 from viam.logging import getLogger
 import time
+import asyncio
 
 MAX_CHANGES = 67
 
@@ -40,17 +41,19 @@ class Receiver(Sensor, EasyResource):
     board: Board
     data_pin: str
 
-    rx_tolerance = 80
+    rx_tolerance = 0.8
     rx_timings = [0] * (MAX_CHANGES + 1)
     rx_last_timestamp = 0
     rx_change_count = 0
     rx_repeat_count = 0
-
+    rx_pulselength = 3500 # microseconds
+    
     rx_code = 0
     rx_code_timestamp = None
-    rx_proto = None
-    rx_bitlength = None
-    rx_pulselength = None
+
+    stream_task = None
+    _stop_event = asyncio.Event()
+    
     
     @classmethod
     def new(
@@ -99,71 +102,93 @@ class Receiver(Sensor, EasyResource):
             config (ComponentConfig): The new configuration
             dependencies (Mapping[ResourceName, ResourceBase]): Any dependencies (both implicit and explicit)
         """
+        if self.stream_task is not None:
+            self._stop_stream_task()
+
         attrs = config.attributes.fields
         board_name = attrs["board"].string_value
         self.board = dependencies[Board.get_resource_name(board_name)]
         self.data_pin = attrs["data_pin"].string_value
         self.gpio = self.board.gpio_pin_by_name(self.data_pin)
+        if "expected_pulse_length" in attrs:
+            self.rx_pulselength = int(attrs["expected_pulse_length"].number_value)
+        if "noise_tolerance" in attrs:
+            self.rx_tolerance = attrs["noise_tolerance"].number_value
+
+        self._stop_event.clear()
+        self.stream_task = asyncio.create_task(self._process_stream_ticks())
         return super().reconfigure(config, dependencies)
     
-    async def receive_code(self):
-        data_interrupt = self.board.DigitalInterrupt(name=self.data_pin)
-        rising_edge_ts = -1
-        falling_edge_ts = -1
-        last_pulse_start_ts = -1
-        last_pulse_end_ts = -1
-        code = 0
-        async for tick in await self.board.stream_ticks([data_interrupt]):
-            # self.logger.info(f"Tick: {tick.time / 1000} microseconds {tick.high}")
-            if tick.high:
-                rising_edge_ts = tick.time
-            else:
-                falling_edge_ts = tick.time
+    def _stop_stream_task(self):
+        self._stop_event.set()
+        if self.stream_task is not None:
+            self.stream_task.cancel()
+            self.stream_task = None
+    
+    async def _process_stream_ticks(self):
+        try:
+            data_interrupt = await self.board.digital_interrupt_by_name(name=self.data_pin)
+            rising_edge_ts = -1
+            falling_edge_ts = -1
+            last_pulse_start_ts = -1
+            last_pulse_end_ts = -1
+            code = 0
+            async for tick in await self.board.stream_ticks([data_interrupt]):
+                if self._stop_event.is_set():
+                    break
+                # self.logger.info(f"Tick: {tick.time / 1000} microseconds {tick.high}")
+                if tick.high:
+                    rising_edge_ts = tick.time
+                else:
+                    falling_edge_ts = tick.time
+                    
+                if falling_edge_ts > rising_edge_ts:
+                    # pulse has ended
+                    high_duration = falling_edge_ts - rising_edge_ts
+                    high_duration_us = high_duration / 1000
+                    if high_duration_us < self.rx_pulselength * self.rx_tolerance:
+                        # filter out very short pulses as noise
+                        high_duration = -1
+                        rising_edge_ts = -1
+                        falling_edge_ts = -1
+                        continue
+                    
+                    # self.logger.info(f"Wave width: {high_duration / 1000} microseconds")
+                    
                 
-            if falling_edge_ts > rising_edge_ts:
-                # pulse has ended
-                high_duration = falling_edge_ts - rising_edge_ts
-                if high_duration < 2500000:
-                    # filter out very short pulses as noise
-                    high_duration = -1
-                    rising_edge_ts = -1
-                    falling_edge_ts = -1
-                    continue
-                
-                # self.logger.info(f"Wave width: {high_duration / 1000} microseconds")
-                
-            
-                distance_from_last_pulse_end = rising_edge_ts - last_pulse_end_ts
+                    distance_from_last_pulse_end_us = (rising_edge_ts - last_pulse_end_ts) / 1000
 
-                if last_pulse_end_ts != -1:
+                    if last_pulse_end_ts != -1:
 
-                    last_pulse_duration = last_pulse_end_ts - last_pulse_start_ts
+                        last_pulse_duration_us = (last_pulse_end_ts - last_pulse_start_ts) / 1000
 
-                    self.logger.info(f"Last pulse duration: {last_pulse_duration / 1000} us, distance from last pulse {distance_from_last_pulse_end / 1000} us")
-                    if distance_from_last_pulse_end > 15000000:
-                        # sync signal, set the reading
-                        self.rx_code = code
-                        self.logger.info(f"Sync signal, final code: {format(code, f'#024b')}")
-                        code = 0
-                    else:
-                        # data signal
+                        self.logger.info(f"Last pulse duration: {last_pulse_duration_us} us, distance from last pulse {distance_from_last_pulse_end_us} us")
+                        if distance_from_last_pulse_end_us > 6 * self.rx_pulselength:
+                            # sync signal, set the reading
+                            self.rx_code = code
+                            self.rx_code_timestamp = last_pulse_end_ts
+                            self.logger.info(f"Sync signal, final code: {format(code, f'#024b')}")
+                            code = 0
+                        else:
+                            # data signal
 
-                        # short high followed by long low is a 0
-                        if last_pulse_duration < 6000000 and distance_from_last_pulse_end > 6000000:
-                            code <<= 1
-                            self.logger.info(f"Signal 0, Binary code: {format(code, f'#024b')}")
+                            # short high followed by long low is a 0
+                            if last_pulse_duration_us < 2 * self.rx_pulselength and distance_from_last_pulse_end_us > 2 * self.rx_pulselength:
+                                code <<= 1
+                                self.logger.info(f"Signal 0, Binary code: {format(code, f'#024b')}")
 
-                        # long high followed by short low is a 1
-                        if last_pulse_duration > 6000000 and distance_from_last_pulse_end < 6000000:
-                            code <<= 1
-                            code |= 1
-                            self.logger.info(f"Signal 1, Binary code: {format(code, f'#024b')}")
-                
-                last_pulse_start_ts = rising_edge_ts
-                last_pulse_end_ts = falling_edge_ts
-
-            
-                
+                            # long high followed by short low is a 1
+                            if last_pulse_duration_us > 2 * self.rx_pulselength and distance_from_last_pulse_end_us < 2 * self.rx_pulselength:
+                                code <<= 1
+                                code |= 1
+                                self.logger.info(f"Signal 1, Binary code: {format(code, f'#024b')}")
+                    
+                    last_pulse_start_ts = rising_edge_ts
+                    last_pulse_end_ts = falling_edge_ts
+        except asyncio.CancelledError:
+            pass
+        except Exception as e:
+            self.logger.error(f"Error streaming ticks: {e}")
             
     async def get_readings(
         self,
@@ -173,6 +198,9 @@ class Receiver(Sensor, EasyResource):
     ) -> Mapping[str, SensorReading]:
         return {"code": str(self.rx_code)}
 
+    async def close(self):
+        self._stop_stream_task()
+
     async def do_command(
         self,
         command: Mapping[str, ValueTypes],
@@ -181,10 +209,7 @@ class Receiver(Sensor, EasyResource):
         **kwargs
     ) -> Mapping[str, ValueTypes]:
         
-        if command.get("receive"):
-            await self.receive_code()
-            return {"status": "received"}
-        return {}
+        raise NotImplementedError()
 
     async def get_geometries(
         self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None
